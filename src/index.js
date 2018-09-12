60aa63c20530837e4b6d2ae2cdf5709b50dc6c48
evgenijkorob <evgenij.k.wie@gmail.com>
2018-09-12 22:27:22 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var info = equation.split(" ");
+  var a = +info[0];
+  var b = +info.slice(3, 5).join("");
+  var c = +info.slice(7, 9).join("");
+  var d = b * b - 4 * a * c;
+  var result = [];
+  result.push(Math.round((-b - Math.sqrt(d)) / (2 * a)));
+  result.push(Math.round((-b + Math.sqrt(d)) / (2 * a)));
+  result.sort((a, b) => a - b);
+  return result;
 }
